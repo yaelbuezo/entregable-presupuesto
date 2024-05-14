cacd55aecfb588baeaf32935b673fff4fe873a21
Yael Buezo <yaelbuezo@gmail.com>
2024-05-14 18:24:41 -0300

    second commit

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
-// pedir presupuesto inicial 
-let presupuesto = Number(prompt('De cuánto es su presupuesto esta semana?'))
-console.log('Su presupuesto es ' + presupuesto);
-
-if(isNaN(presupuesto)) {
-  alert('El monto ingresado es inválido. Por favor, ingresar un valor numérico.')
-  // volver a preguntar presupuesto
-  presupuesto = Number(prompt('De cuánto es su presupuesto esta semana?'))
-  console.log('Su presupuesto es ' + presupuesto);
-}
-
-// definir gasto: nombre y monto
-let gasto = {}
-function nuevoGasto() {
-  gasto.nombre = prompt('Ingresa el nombre del gasto')
-  gasto.monto = Number(prompt('Ingresa el monto gastado'))
-  
-  if(isNaN(gasto.monto)) {
+// pedir presupuesto
+let presupuesto
+function pedirPresupuesto() {
+  presupuesto = prompt('¿Cuál es su presupuesto esta semana?')
+  while (isNaN(presupuesto) || presupuesto <= 0) {
     alert('El monto ingresado es inválido. Por favor, ingresar un valor numérico.')
-    gasto.monto = Number(prompt('Ingresa el monto gastado'))
+    presupuesto = prompt('¿Cuál es su presupuesto esta semana?')
   }
-  return gasto
+  presupuestoHTML()
 }
-console.log(gasto)
+
+// imprimir presupuesto
+function presupuestoHTML() {
+  const presupuestoInicial = document.getElementById('presupuesto')
+  presupuestoInicial.textContent = presupuesto;
+}
+pedirPresupuesto()
+
+// agregar gasto
+const form = document.querySelector('#agregar-gasto')
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  
+  const nombre = document.querySelector('#nombre').value;
+  const monto = Number(document.querySelector('#monto').value);
+  
+  if(nombre === '' || monto === '') {
+    alert('Ambos campos son obligatorios');
+    return;
+  } else if(monto <= 0 || isNaN(monto)) {
+    alert('El monto ingresado es inválido. Por favor, ingresar un valor numérico.');
+    return;
+  }
+  
+  // genera objeto gasto
+  const gasto = {nombre, monto}
+});
 
 // definir array gastado
 const gastado = []
@@ -40,6 +53,17 @@ const ingresarGastos = () => {
     }
   } while (total <= presupuesto);
 }
+
+// lista de gastos
+// listaGastos(gastado) {
+//   gastado.forEach(gasto => {
+//     const {monto, nombre} = gasto;
+
+//     const nuevoGasto = document.createElement('li');
+//     nuevoGasto.className = 'list-group-item d-flex align-items-center';
+//     nuevoGasto.innerHTML = `${nombre} <span>$ ${monto}</span>`;
+//   });
+// }
 
 // calcular presupuesto restante
 function calculaRestante(presupuesto) {
