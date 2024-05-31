4806f184e94acdffc7c2c66e995a898cc0d06ada
Yael Buezo <yaelbuezo@gmail.com>
2024-05-31 16:36:22 -0300

    thrid commit

--- main.js
+++ main.js
@@ -6,18 +6,24 @@ function pedirPresupuesto() {
     alert('El monto ingresado es inválido. Por favor, ingresar un valor numérico.')
     presupuesto = prompt('¿Cuál es su presupuesto esta semana?')
   }
-  presupuestoHTML()
 }
 
 // imprimir presupuesto
-function presupuestoHTML() {
-  const presupuestoInicial = document.getElementById('presupuesto')
-  presupuestoInicial.textContent = presupuesto;
-}
-pedirPresupuesto()
+document.addEventListener("DOMContentLoaded", function() { 
+  pedirPresupuesto()
+  const presupuestoInicial = document.getElementById('inicial');
+  presupuestoInicial.innerHTML = `${presupuesto}`;
+});
+
 
 // agregar gasto
+let total = 0;
 const form = document.querySelector('#agregar-gasto')
+
+function limpiarForm() {
+  form.reset();
+}
+
 form.addEventListener('submit', e => {
   e.preventDefault();
   
@@ -28,50 +34,60 @@ form.addEventListener('submit', e => {
     alert('Ambos campos son obligatorios');
     return;
   } else if(monto <= 0 || isNaN(monto)) {
-    alert('El monto ingresado es inválido. Por favor, ingresar un valor numérico.');
+    alert('El monto ingresado es inválido. Por favor, ingrese un valor numérico.');
     return;
   }
   
-  // genera objeto gasto
-  const gasto = {nombre, monto}
+  const nuevoGasto = () => {
+    return { nombre, monto }
+  }
+  
+  const gastado = []
+
+  const ingresarGastos = () => {
+    let gasto
+    do {
+      gasto = nuevoGasto()
+      gastado.push(gasto)
+      total += monto
+      
+      if (total > presupuesto) {
+        alert('Presupuesto excedido')
+        gastado.pop() // elimina último gasto (si se excede)
+        total -= monto;
+        return; // exit the loop
+      }
+    } while (gastado <= presupuesto);
+  }
+
+  ingresarGastos()
+
+  // imprimir gastos
+  function listaGastos() {
+    const lista = document.getElementById('lista-gastos')
+
+    gastado.forEach(gasto => {
+      const nuevoGasto = document.createElement('li');
+      nuevoGasto.className = 'list-group-item d-flex align-items-center';
+      nuevoGasto.innerHTML = `${gasto.nombre} - $${gasto.monto}`;
+      lista.appendChild(nuevoGasto)
+    });
+  }
+
+  limpiarForm()
+  listaGastos()
+  const totalGastado = document.getElementById('total')
+  totalGastado.innerHTML = `${total}`;
+
+  // calcular presupuesto restante
+  function calculaRestante(presupuesto, total) {
+    const restante = presupuesto - total;
+    return restante;
+  }
+  const restante = calculaRestante(presupuesto, total)
+
+  // imprimir restante
+  calculaRestante()
+  const presupuestoRestante = document.getElementById('restante');
+  presupuestoRestante.innerHTML = `${restante}`;
 });
-
-// definir array gastado
-const gastado = []
-let total = 0
-
-// ciclo - do while
-const ingresarGastos = () => {
-  do {
-    gasto = nuevoGasto()
-    gastado.push(gasto.monto)
-    total += gasto.monto
-    
-    // if gastado>presupuesto, 'presupuesto excedido'
-    if (total > presupuesto) {
-      alert('Presupuesto excedido')
-    }
-  } while (total <= presupuesto);
-}
-
-// lista de gastos
-// listaGastos(gastado) {
-//   gastado.forEach(gasto => {
-//     const {monto, nombre} = gasto;
-
-//     const nuevoGasto = document.createElement('li');
-//     nuevoGasto.className = 'list-group-item d-flex align-items-center';
-//     nuevoGasto.innerHTML = `${nombre} <span>$ ${monto}</span>`;
-//   });
-// }
-
-// calcular presupuesto restante
-function calculaRestante(presupuesto) {
-  const restante = presupuesto - total;
-  return restante;
-}
-
-ingresarGastos();
-
-restante = calculaRestante(presupuesto)
-console.log('Su presupuesto restante es ' + restante)
